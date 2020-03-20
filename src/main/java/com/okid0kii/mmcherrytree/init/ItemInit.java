2aab7fda4f25e1f0e3e88f99e6a890f87c745040
okid0kii <okid0kii@users.noreply.github.com>
2020-03-20 14:12:23 +0100

    Update ItemInit.java

--- src/main/java/com/okid0kii/mmcherrytree/init/ItemInit.java
+++ src/main/java/com/okid0kii/mmcherrytree/init/ItemInit.java
@@ -1,7 +1,7 @@
 package com.okid0kii.mmcherrytree.init;
 
 import com.okid0kii.mmcherrytree.CherryTreeMod;
-import com.okid0kii.mmcherrytree.CherryTreeMod.MMItemGroup;
+import com.okid0kii.mmcherrytree.CherryTreeMod.MMsItemGroup;
 
 import net.minecraft.item.Item;
 
@@ -13,5 +13,5 @@ public class ItemInit {
 	 
 	public static final DeferredRegister<Item> ITEMS = new DeferredRegister<> (ForgeRegistries.ITEMS, CherryTreeMod.MOD_ID);
 	
-	public static final RegistryObject<Item> DEF_ITEM = ITEMS.register("def_item", () -> new Item(new Item.Properties().group(MMItemGroup.instance)));
+	public static final RegistryObject<Item> DEF_ITEM = ITEMS.register("def_item", () -> new Item(new Item.Properties().group(MMsItemGroup.instance)));
 }
