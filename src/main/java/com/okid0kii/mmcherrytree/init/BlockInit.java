5c6b9db5e5c18e6db98ea23a80b60586c733c804
okid0kii <okid0kii@users.noreply.github.com>
2020-03-21 13:26:07 +0100

    Update BlockInit.java

--- src/main/java/com/okid0kii/mmcherrytree/init/BlockInit.java
+++ src/main/java/com/okid0kii/mmcherrytree/init/BlockInit.java
@@ -1,17 +1,20 @@
 package com.okid0kii.mmcherrytree.init;
 
 import com.okid0kii.mmcherrytree.CherryTreeMod;
+import com.okid0kii.mmcherrytree.objects.blocks.LogBlocks;
 
 import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
 
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
+
 public class BlockInit {
 	 
 	public static final DeferredRegister<Block> BLOCKS = new DeferredRegister<> (ForgeRegistries.BLOCKS, CherryTreeMod.MOD_ID);
 	
-	public static final RegistryObject<Block> CHERRY_LOG = BLOCKS.register("cherry_log", () -> new Block(Block.Properties.create(Material.WOOD)));
+	public static final RegistryObject<Block> CHERRY_LOG = BLOCKS.register("cherry_log", () -> new Block(Block.Properties.from(LogBlocks.CHERRY_LOG)));
+	
+	//public static final RegistryObject<Block> DEF = BLOCKS.register("def", () -> new Block(Block.Properties.create(Material.WOOD)));
 }
