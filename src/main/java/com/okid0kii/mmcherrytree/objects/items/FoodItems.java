270de265517d05381b2d0e8a69724234697a3693
okid0kii <okid0kii@users.noreply.github.com>
2020-03-21 13:26:18 +0100

    Create FoodItems.java

---
+++ src/main/java/com/okid0kii/mmcherrytree/objects/items/FoodItems.java
@@ -0,0 +1,10 @@
+package com.okid0kii.mmcherrytree.objects.items;
+
+import net.minecraft.item.Food;
+
+
+		public class FoodItems {
+			
+		public static final Food CHERRY_FRUIT = (new Food.Builder().hunger(6).saturation(0.2F).setAlwaysEdible()).build();
+
+		}
