c21deb65646d93bc28ebc1da1470fe7fa60e6090
okid0kii <okid0kii@users.noreply.github.com>
2020-03-21 13:26:15 +0100

    Create LogBlocks.java

---
+++ src/main/java/com/okid0kii/mmcherrytree/objects/blocks/LogBlocks.java
@@ -0,0 +1,19 @@
+package com.okid0kii.mmcherrytree.objects.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.LogBlock;
+import net.minecraft.block.material.MaterialColor;
+
+public class LogBlocks extends LogBlock {
+
+	
+	
+	public LogBlocks(MaterialColor verticalColorIn, Properties properties) {
+		super(verticalColorIn, properties);
+
+	}
+
+	public static final LogBlock CHERRY_LOG = new LogBlocks(MaterialColor.WOOD, Block.Properties.from(Blocks.OAK_LOG));
+	
+}
