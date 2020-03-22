764a0a6915fb8cf71bbc136bcf6f1c0f5012aeb6
okid0kii <okid0kii@users.noreply.github.com>
2020-03-22 09:05:27 +0100

    Update BlockInit.java

--- src/main/java/com/okid0kii/mmcherrytree/init/BlockInit.java
+++ src/main/java/com/okid0kii/mmcherrytree/init/BlockInit.java
@@ -1,10 +1,13 @@
 package com.okid0kii.mmcherrytree.init;
 
 import com.okid0kii.mmcherrytree.CherryTreeMod;
-import com.okid0kii.mmcherrytree.objects.blocks.LogBlocks;
+
 
 import net.minecraft.block.Block;
-
+import net.minecraft.block.Blocks;
+import net.minecraft.block.LogBlock;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.material.MaterialColor;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -14,7 +17,8 @@ public class BlockInit {
 	 
 	public static final DeferredRegister<Block> BLOCKS = new DeferredRegister<> (ForgeRegistries.BLOCKS, CherryTreeMod.MOD_ID);
 	
-	public static final RegistryObject<Block> CHERRY_LOG = BLOCKS.register("cherry_log", () -> new Block(Block.Properties.from(LogBlocks.CHERRY_LOG)));
+	public static final RegistryObject<Block> CHERRY_LOG = BLOCKS.register("cherry_log", () -> new LogBlock(MaterialColor.WOOD, Block.Properties.from(Blocks.OAK_LOG)));
 	
-	//public static final RegistryObject<Block> DEF = BLOCKS.register("def", () -> new Block(Block.Properties.create(Material.WOOD)));
+	//public static final RegistryObject<Block> CHERRY_LOG = BLOCKS.register("cherry_log", () -> new LogBlock(Block.Properties.from(LogBlocks.CHERRY_LOG)));
+	public static final RegistryObject<Block> DEF = BLOCKS.register("def", () -> new Block(Block.Properties.create(Material.WOOD)));
 }
