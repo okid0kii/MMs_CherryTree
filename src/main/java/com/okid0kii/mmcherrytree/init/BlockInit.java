2c8446e148c4be5c7e410095efcf23457f216d6c
okid0kii <okid0kii@users.noreply.github.com>
2020-03-22 23:15:36 +0100

    Update BlockInit.java

--- src/main/java/com/okid0kii/mmcherrytree/init/BlockInit.java
+++ src/main/java/com/okid0kii/mmcherrytree/init/BlockInit.java
@@ -7,7 +7,8 @@ import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.FenceBlock;
 import net.minecraft.block.LogBlock;
-
+import net.minecraft.block.SlabBlock;
+import net.minecraft.block.StairsBlock;
 import net.minecraft.block.material.MaterialColor;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
@@ -21,23 +22,20 @@ public class BlockInit {
 	
 //LOGBLOCKS "RotatedPillarBlock"
 	public static final RegistryObject<Block> CHERRY_LOG = BLOCKS.register("cherry_log", () -> new LogBlock(MaterialColor.WOOD, Block.Properties.from(Blocks.OAK_LOG)));
-	
 	public static final RegistryObject<Block> STRIPPED_CHERRY_LOG = BLOCKS.register("stripped_cherry_log", () -> new LogBlock(MaterialColor.WOOD,Block.Properties.from(Blocks.STRIPPED_OAK_LOG)));
 	
 //BLOCKS "Simple Block"
 	public static final RegistryObject<Block> CHERRY_WOOD = BLOCKS.register("cherry_wood", () -> new Block(Block.Properties.from(Blocks.OAK_WOOD)));
-	
 	public static final RegistryObject<Block> STRIPPED_CHERRY_WOOD = BLOCKS.register("stripped_cherry_wood", () -> new Block(Block.Properties.from(Blocks.STRIPPED_OAK_WOOD)));
-	
 	public static final RegistryObject<Block> CHERRY_PLANKS = BLOCKS.register("cherry_planks", () -> new Block(Block.Properties.from(Blocks.OAK_PLANKS)));
 	
-//DECORATION BLOCKS
-	public static final RegistryObject<Block> CHERRY_LOG_FENCE = BLOCKS.register("cherry_log_fence", () -> new FenceBlock(Block.Properties.from(Blocks.OAK_FENCE)));
+//CONSTRUCTION BLOCKS
+	public static final RegistryObject<Block> CHERRY_WOOD_FENCE = BLOCKS.register("cherry_wood_fence", () -> new FenceBlock(Block.Properties.from(Blocks.OAK_FENCE)));
 	public static final RegistryObject<Block> CHERRY_FENCE = BLOCKS.register("cherry_fence", () -> new FenceBlock(Block.Properties.from(Blocks.OAK_FENCE)));
 	
+	public static final RegistryObject<Block> CHERRY_SLAB = BLOCKS.register("cherry_slab", () -> new SlabBlock(Block.Properties.from(Blocks.OAK_SLAB)));
+	public static final RegistryObject<Block> CHERRY_WOOD_SLAB = BLOCKS.register("cherry_wood_slab", () -> new SlabBlock(Block.Properties.from(Blocks.OAK_SLAB)));
 	
-	//public static final RegistryObject<Block> CHERRY_FENCE = BLOCKS.register("cherry_fence", () -> new FenceBlock(Block.Properties.create(Material.WOOD, MaterialColor.WOOD)));
-	
-
-	//public static final RegistryObject<Block> DEF = BLOCKS.register("def", () -> new Block(Block.Properties.create(Material.WOOD)));
+	public static final RegistryObject<Block> CHERRY_STAIRS = BLOCKS.register("cherry_stairs", () -> new StairsBlock(() -> BlockInit.CHERRY_PLANKS.get().getDefaultState(), Block.Properties.from(Blocks.OAK_STAIRS)));
+	public static final RegistryObject<Block> CHERRY_WOOD_STAIRS = BLOCKS.register("cherry_wood_stairs", () -> new StairsBlock(() -> BlockInit.CHERRY_WOOD.get().getDefaultState(), Block.Properties.from(Blocks.OAK_STAIRS)));
 }
