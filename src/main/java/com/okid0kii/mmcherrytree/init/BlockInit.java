3a5aef1cd3b3a547474ca9e0219f99eb251ec0d4
okid0kii <okid0kii@users.noreply.github.com>
2020-03-29 14:33:30 +0200

    Update BlockInit.java

--- src/main/java/com/okid0kii/mmcherrytree/init/BlockInit.java
+++ src/main/java/com/okid0kii/mmcherrytree/init/BlockInit.java
@@ -1,18 +1,25 @@
 package com.okid0kii.mmcherrytree.init;
 
 import com.okid0kii.mmcherrytree.CherryTreeMod;
-
+import com.okid0kii.mmcherrytree.objects.blocks.SculptureBlock;
+import com.okid0kii.mmcherrytree.objects.items.CherryTree;
 
 import net.minecraft.block.Block;
+
 import net.minecraft.block.Blocks;
 import net.minecraft.block.FenceBlock;
+import net.minecraft.block.GlazedTerracottaBlock;
+import net.minecraft.block.LeavesBlock;
 import net.minecraft.block.LogBlock;
+import net.minecraft.block.SaplingBlock;
 import net.minecraft.block.SlabBlock;
 import net.minecraft.block.StairsBlock;
 import net.minecraft.block.material.MaterialColor;
+
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+
 
 
 public class BlockInit {
@@ -38,4 +45,29 @@ public class BlockInit {
 	
 	public static final RegistryObject<Block> CHERRY_STAIRS = BLOCKS.register("cherry_stairs", () -> new StairsBlock(() -> BlockInit.CHERRY_PLANKS.get().getDefaultState(), Block.Properties.from(Blocks.OAK_STAIRS)));
 	public static final RegistryObject<Block> CHERRY_WOOD_STAIRS = BLOCKS.register("cherry_wood_stairs", () -> new StairsBlock(() -> BlockInit.CHERRY_WOOD.get().getDefaultState(), Block.Properties.from(Blocks.OAK_STAIRS)));
-}
+
+//VEGETABLES
+	public static final RegistryObject<Block> CHERRY_LEAVES = BLOCKS.register("cherry_leaves", () -> new LeavesBlock(Block.Properties.from(Blocks.OAK_LEAVES)));
+
+//DECORATION
+	public static final RegistryObject<Block> CHERRY_CARVED = BLOCKS.register("cherry_carved",  () -> new GlazedTerracottaBlock(Block.Properties.from(Blocks.OAK_PLANKS)));
+	public static final RegistryObject<Block> CHERRY_TANUKI = BLOCKS.register("cherry_tanuki", () -> new SculptureBlock(Block.Properties.from(Blocks.OAK_PLANKS)));
+	
+//BLOCKITEMS
+	public static final DeferredRegister<Block> NO_BLOCKS = new DeferredRegister<> (ForgeRegistries.BLOCKS, CherryTreeMod.MOD_ID);
+	public static final RegistryObject<Block> CHERRY_SAPLING = NO_BLOCKS.register("cherry_sapling", () -> new SaplingBlock(new CherryTree(), Block.Properties.from(Blocks.OAK_SAPLING)) {});
+
+
+}  
+
+	
+
+
+
+
+
+
+
+
+
+
