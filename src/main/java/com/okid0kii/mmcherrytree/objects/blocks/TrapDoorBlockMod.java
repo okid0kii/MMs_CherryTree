34e9e83a924a7da1c12304e6cbf3f3aef77fe647
okid0kii <okid0kii@users.noreply.github.com>
2020-04-01 16:50:39 +0200

    Create TrapDoorBlockMod.java

---
+++ src/main/java/com/okid0kii/mmcherrytree/objects/blocks/TrapDoorBlockMod.java
@@ -0,0 +1,17 @@
+package com.okid0kii.mmcherrytree.objects.blocks;
+
+import net.minecraft.block.IWaterLoggable;
+import net.minecraft.block.TrapDoorBlock;
+
+
+
+
+
+public class TrapDoorBlockMod extends TrapDoorBlock implements IWaterLoggable {
+
+	
+	public TrapDoorBlockMod(Properties properties) {
+		super(properties);
+				
+	}
+}
