7c301f8d37770a2f1f409d1cb3d782e9b52df795
okid0kii <okid0kii@users.noreply.github.com>
2020-04-01 12:40:11 +0200

    Create DoorBlockMod.java

---
+++ src/main/java/com/okid0kii/mmcherrytree/objects/blocks/DoorBlockMod.java
@@ -0,0 +1,12 @@
+package com.okid0kii.mmcherrytree.objects.blocks;
+
+import net.minecraft.block.DoorBlock;
+
+public class DoorBlockMod extends DoorBlock {
+
+	public DoorBlockMod(Properties builder) {
+		super(builder);
+		
+	}
+
+}
