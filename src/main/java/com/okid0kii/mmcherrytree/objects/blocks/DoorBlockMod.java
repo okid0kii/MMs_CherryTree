aaa00a4000bdc6e07317ae2145ad5e54ec0c0b35
okid0kii <okid0kii@users.noreply.github.com>
2020-04-03 08:47:43 +0200

    Update DoorBlockMod.java

--- src/main/java/com/okid0kii/mmcherrytree/objects/blocks/DoorBlockMod.java
+++ src/main/java/com/okid0kii/mmcherrytree/objects/blocks/DoorBlockMod.java
@@ -2,6 +2,7 @@ package com.okid0kii.mmcherrytree.objects.blocks;
 
 import net.minecraft.block.DoorBlock;
 
+
 public class DoorBlockMod extends DoorBlock {
 
 	public DoorBlockMod(Properties builder) {
