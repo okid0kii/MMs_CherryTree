f122a90faf76e6211f637b4071c0c8db7c9355bd
okid0kii <okid0kii@users.noreply.github.com>
2020-04-03 08:47:45 +0200

    Create PaneBlockMod.java

---
+++ src/main/java/com/okid0kii/mmcherrytree/objects/blocks/PaneBlockMod.java
@@ -0,0 +1,13 @@
+package com.okid0kii.mmcherrytree.objects.blocks;
+
+import net.minecraft.block.IWaterLoggable;
+import net.minecraft.block.PaneBlock;
+
+public class PaneBlockMod extends PaneBlock implements IWaterLoggable {
+
+	public PaneBlockMod(Properties builder) {
+		super(builder);
+		
+	}
+
+}
